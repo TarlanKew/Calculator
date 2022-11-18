54831dd248618da1fe9a90c75a007133b3b0ccbb
TarlanKew <tarlan.quliev@icloud.com>
2022-11-18 15:12:46 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Tarlan Quliev on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
